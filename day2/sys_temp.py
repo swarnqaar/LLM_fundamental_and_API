ee6859c7c123a6ce7ed23a4d0d34ea7342a80012
Shubham Kumar <shubamkumar3039@gmail.com>
2026-09-09 18:54:55 +0530

    changed my model

--- day2/sys_temp.py
+++ day2/sys_temp.py
@@ -11,7 +11,7 @@ if not my_api_key:
 
 client=Groq(api_key=my_api_key)
 
-model="llama-3.3-70b-versatile"
+model="openai/gpt-oss-120b"
 role="user"
 prompt="Suggest a name for my clothing company"
 # SYSTEM
